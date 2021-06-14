217998fc6dae4c7464ec34c5018bf9c57923dc34
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-06-15 01:17:18 +0530

    Added userSchema for data in DB

---
+++ models/userSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema = {
+  name: { type: String, required: true },
+  email: { type: String, required: true},
+  mobile: { type: Number, requried: true},
+  password: {type: String, required: true},
+  cpassword: {type: String, required: true},
+};
+
+const User = mongoose.model('USER', userSchema);
+module.exports = User;
