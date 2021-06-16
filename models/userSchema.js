687fa756595ccfd6d43bc88c9847cd45c3fbf751
Sanjay Rajpurohit <sanjay.rajpurohit@embibe.com>
2021-06-16 10:18:46 +0530

    Added JWT authentication and saved the token in cookie.

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   mobile: { type: Number, requried: true },
   password: { type: String, required: true },
   cpassword: { type: String, required: true },
+  tokens: [{ token: { type: String, required: true } }],
 });
 
 // hashing password
@@ -16,6 +18,30 @@ userSchema.pre("save", async function (req, res, next) {
   }
   next();
 });
+
+// we're generating auth token by using instance method
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    /**
+     * jwt.sign requires two arguments 1 payload obj {}, secret key
+     * payload must be unique
+     * secretkey
+     * */
+    let generatedToken = jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+      },
+      process.env.SECRET_KEY
+    );
+    // saving the token to the db
+    this.tokens = this.tokens.concat({ token: generatedToken });
+    await this.save();
+    return generatedToken; // returning the token so that it can be sent to FE
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const User = mongoose.model("USER", userSchema);
 module.exports = User;
